14abd9193bf53844996cafaff3d07c492f17408d
Santiago Beltran <s.beltran10@uniandes.edu.co>
2017-05-09 09:56:49 -0500

    Nueva pantalla

--- public/javascripts/main-detalle.js
+++ public/javascripts/main-detalle.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-    for (var i = 0; i < 10; i++) {
-        var itemServicio = $('<div class="item-servicio"></div>');
+    for (var i = 0; i < 15; i++) {
+        var itemServicio = $('<div id="item-' + i + '" class="item-servicio"></div>');
         var item = $('<div class="row row-imagen"><div class="col-lg-12"></div></div>');
-        var iconoServicio = $('<div id="imagen-servicio-'+i+'" class="imagen-servicio"><div id="txt-servicio-' + i + '" class="txt-servicio">B1</div></div>');
+        var iconoServicio = $('<div id="imagen-servicio-' + i + '" class="imagen-servicio"><div id="txt-servicio-' + i + '" class="txt-servicio">B1</div></div>');
         item.append(iconoServicio);
 
-        var inc = Math.floor(Math.random() * (i+1)) + 1;
+        var inc = Math.floor(Math.random() * (i + 1)) + 1;
 
         var infoTiempo = $('<div class="row row-tiempo"><div class="col-lg-12">Se encuentra a <span class="txt-tiempo">' + (i + inc) + ' minutos</span></div></div>');
         item.append(infoTiempo);
@@ -38,30 +38,46 @@ $(document).ready(function () {
         var randServicio = Math.floor(Math.random() * 6) + 1;
         if (randServicio === 1) {
             $('#txt-servicio-' + i).html("B1");
-            colorservicio("B1",$('#imagen-servicio-' + i));
+            colorservicio("B1", $('#imagen-servicio-' + i));
         }
         else if (randServicio === 2) {
             $('#txt-servicio-' + i).html("B5");
-            colorservicio("B5",$('#imagen-servicio-' + i));
+            colorservicio("B5", $('#imagen-servicio-' + i));
         }
         else if (randServicio === 3) {
             $('#txt-servicio-' + i).html("G11");
-            colorservicio("G11",$('#imagen-servicio-' + i));
+            colorservicio("G11", $('#imagen-servicio-' + i));
         }
         else if (randServicio === 4) {
             $('#txt-servicio-' + i).html("F14");
-            colorservicio("F14",$('#imagen-servicio-' + i));
+            colorservicio("F14", $('#imagen-servicio-' + i));
         }
         else if (randServicio === 5) {
             $('#txt-servicio-' + i).html("H74");
-            colorservicio("H74",$('#imagen-servicio-' + i));
+            colorservicio("H74", $('#imagen-servicio-' + i));
         }
         else if (randServicio === 6) {
             $('#txt-servicio-' + i).html("J72");
-            colorservicio("J72",$('#imagen-servicio-' + i));
+            colorservicio("J72", $('#imagen-servicio-' + i));
         }
 
-        
+
     }
 
+    $('#boton-filtrar').click(function (e) { e.preventDefault(); filtrar($("#filtrar-servicio").val()); return false; });
 });
+
+function filtrar(texto) {
+
+
+    for (var i = 0; i < 15; i++) {
+        $("#item-" + i).show();
+    }
+
+    for (var i = 0; i < 15; i++) {
+        if (!$("#txt-servicio-" + i).html().startsWith(texto)) {
+            console.log($("#txt-servicio-" + i).html());
+            $("#item-" + i).hide();
+        }
+    }
+}
