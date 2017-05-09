1f6b5c3dd83a05ec686868ab489cedfc663420a2
Santiago Beltran <s.beltran10@uniandes.edu.co>
2017-05-09 08:20:36 -0500

    Nueva pantalla

--- public/javascripts/main-detalle.js
+++ public/javascripts/main-detalle.js
@@ -1,55 +1,38 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    var itemServicio = $('<div class="item-servicio"></div>');
-    var item = $('<div class="row row-imagen"><div class="col-lg-12"></div></div>');
-    var iconoServicio = $('<div class="imagen-servicio"><div class="txt-servicio">B1</div></div>');
-    item.append(iconoServicio);
+    for (var i = 0; i < 5; i++) {
+        var itemServicio = $('<div class="item-servicio"></div>');
+        var item = $('<div class="row row-imagen"><div class="col-lg-12"></div></div>');
+        var iconoServicio = $('<div class="imagen-servicio"><div class="txt-servicio">B1</div></div>');
+        item.append(iconoServicio);
 
-    var infoTiempo = $('<div class="row row-tiempo"><div class="col-lg-12">Se encuentra a <span class="txt-tiempo">10 minutos</span></div></div>');
-    item.append(infoTiempo);
-    var infoOcupacion = $('<div class="row row-ocupacion"><div class="col-lg-12">Tiene una ocupacion <span class="nivel-ocupacion">alta</span></div></div>');
-    item.append(infoOcupacion);
+        var inc = Math.floor(Math.random() * i) + 1;
 
-    itemServicio.append(item);
-    $("#lista-servicios").append(itemServicio);
+        var infoTiempo = $('<div class="row row-tiempo"><div class="col-lg-12">Se encuentra a <span class="txt-tiempo">'+(i+1+inc)+ ' minutos</span></div></div>');
+        item.append(infoTiempo);
 
-    var itemServicio = $('<div class="item-servicio"></div>');
-    var item = $('<div class="row row-imagen"><div class="col-lg-12"></div></div>');
-    var iconoServicio = $('<div class="imagen-servicio"><div class="txt-servicio">B1</div></div>');
-    item.append(iconoServicio);
+        var rand = Math.floor(Math.random() * 4) + 1;
+        console.log(rand);
+        if (rand === 1) {
+            var infoOcupacion = $('<div class="row row-ocupacion"><div class="col-lg-12">Tiene una ocupación <span class="nivel-ocupacion-baja">baja</span></div></div>');
+            item.append(infoOcupacion);
+        }
+        else if (rand === 2) {
+            var infoOcupacion = $('<div class="row row-ocupacion"><div class="col-lg-12">Tiene una ocupación <span class="nivel-ocupacion-media">media</span></div></div>');
+            item.append(infoOcupacion);
+        }
+        else if (rand === 3) {
+            var infoOcupacion = $('<div class="row row-ocupacion"><div class="col-lg-12">Tiene una ocupación <span class="nivel-ocupacion-alta">alta</span></div></div>');
+            item.append(infoOcupacion);
+        }
+        else if (rand === 4) {
+            var infoOcupacion = $('<div class="row row-ocupacion"><div class="col-lg-12">Tiene una ocupación <span class="nivel-ocupacion-muy-alta">muy alta</span></div></div>');
+            item.append(infoOcupacion);
+        }
 
-    var infoTiempo = $('<div class="row row-tiempo"><div class="col-lg-12">Se encuentra a <span class="txt-tiempo">10 minutos</span></div></div>');
-    item.append(infoTiempo);
-    var infoOcupacion = $('<div class="row row-ocupacion"><div class="col-lg-12">Tiene una ocupacion <span class="nivel-ocupacion">alta</span></div></div>');
-    item.append(infoOcupacion);
 
-    itemServicio.append(item);
-    $("#lista-servicios").append(itemServicio);
+        itemServicio.append(item);
+        $("#lista-servicios").append(itemServicio);
+    }
 
-    var itemServicio = $('<div class="item-servicio"></div>');
-    var item = $('<div class="row row-imagen"><div class="col-lg-12"></div></div>');
-    var iconoServicio = $('<div class="imagen-servicio"><div class="txt-servicio">B1</div></div>');
-    item.append(iconoServicio);
-
-    var infoTiempo = $('<div class="row row-tiempo"><div class="col-lg-12">Se encuentra a <span class="txt-tiempo">10 minutos</span></div></div>');
-    item.append(infoTiempo);
-    var infoOcupacion = $('<div class="row row-ocupacion"><div class="col-lg-12">Tiene una ocupacion <span class="nivel-ocupacion">alta</span></div></div>');
-    item.append(infoOcupacion);
-
-    itemServicio.append(item);
-    $("#lista-servicios").append(itemServicio);
-
-    var itemServicio = $('<div class="item-servicio"></div>');
-    var item = $('<div class="row row-imagen"><div class="col-lg-12"></div></div>');
-    var iconoServicio = $('<div class="imagen-servicio"><div class="txt-servicio">B1</div></div>');
-    item.append(iconoServicio);
-
-    var infoTiempo = $('<div class="row row-tiempo"><div class="col-lg-12">Se encuentra a <span class="txt-tiempo">10 minutos</span></div></div>');
-    item.append(infoTiempo);
-    var infoOcupacion = $('<div class="row row-ocupacion"><div class="col-lg-12">Tiene una ocupacion <span class="nivel-ocupacion">alta</span></div></div>');
-    item.append(infoOcupacion);
-
-    itemServicio.append(item);
-    $("#lista-servicios").append(itemServicio);
-  
 });
